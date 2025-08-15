1b28105f05563e98891de9e06319fc6d3bb38fbe
David Kim <khk980703@hotmail.com>
2025-08-15 22:38:14 +1200

    set up next js app

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h2>Welcome to Pokemon Card Trading App!</h2>
+        <p>Sign in to start trading.</p>
+      </div>
+    </main>
+  )
+}
